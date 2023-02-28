8055d9fbc4a0a719029a268a0afd5d1b6d5668c1
Logan Gray <loganallengray@gmail.com>
2023-02-28 10:06:08 -0600

    phase 2 completed, user can now name adventurer

--- Program.cs
+++ Program.cs
@@ -43,7 +43,10 @@ namespace Quest
             int maxAwesomeness = 100;
 
             // Make a new "Adventurer" object using the "Adventurer" class
-            Adventurer theAdventurer = new Adventurer("Jack");
+            // Get the user's choice for what they want their adventurer to be named
+            Console.WriteLine("What is your name, adventurer? :");
+            string adventurerName = Console.ReadLine();
+            Adventurer theAdventurer = new Adventurer(adventurerName);
 
             // A list of challenges for the Adventurer to complete
             // Note we can use the List class here because have the line "using System.Collections.Generic;" at the top of the file.
